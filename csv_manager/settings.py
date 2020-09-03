a7df95aff5e51b77cd77c32d5f02609154132785
APFirebolt <aspper20@gmail.com>
2020-09-03 13:22:00 -0700

    Debug settings

--- csv_manager/settings.py
+++ csv_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't&-7pgw#lo=4$(q=lo@1hyvznfn3d*=w_7@hga*upx9@w4pa8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','83.136.219.54',]
 
